8dfbc6a79617d2988081c95274f323482a2adea8
Waderjohnson <104044129+Waderjohnson@users.noreply.github.com>
2022-05-19 13:32:58 -0600

    added File Folder "JavaScript Projects"

---
+++ Basic JavaScript Projects/Project1_expressions_alert/Js/main.js
@@ -0,0 +1,8 @@
+var family = "Johnsons ", Dad="Robert ", Mom="Laurie ",    //Defines a Variable string
+Son="Wade", Daughter1="Leanna ", Daughter2="Krista ";   
+
+document.write(Daughter1+"and "+Daughter2, "<br>Don't argue with me<br>");  //Writes out concatenated variables and added text
+
+document.write(1 + 1, " sisters.");     //An expression and writes out the added numbers + text
+
+window.alert("Who are the favorite kiddos?"); //Window alert asking the question
